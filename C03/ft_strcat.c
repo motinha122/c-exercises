4f8553eb9a674dce163bf3a2c3ca885825b8fc16
motinha122 <timota132@gmail.com>
2025-01-24 21:06:20 -0300

    strcat implementation

--- C03/ft_strcat.c
+++ C03/ft_strcat.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int ft_strcat(char *s1, char *s2)
+int ft_strlen(char *str)
 {
-  
+  int i = 0;
+
+  while (*str)
+  {
+    str++;
+    i++;
+  }
+  return i;
+}
+
+char *ft_strcat(char *dest, char *src)
+{
+  int src_i = 0;
+  int dest_i = ft_strlen(dest);
+  char *ret = dest;
+
+  while (*dest)
+  {
+    dest++;
+  }
+
+  while(src_i < ft_strlen(src)){
+    dest[dest_i + src_i] = src[src_i];
+    src_i++;
+  }
+
+  return ret;
 }
 
 int main(void)
 {
-  char str1[10] = "batataç";
-  char str2[15] = "batata";
+  char str1[10] = "lep";
+  char str2[15] = "japuka";
 
-  printf("%d \n", strcmp(str1, str2));
-  printf("%d \n", ft_strcmp(str1, str2));
-
+  printf("%s \n", strcat(str1, str2));
+  printf("%s \n", ft_strcat(str1, str2));
   return 0;
 }
