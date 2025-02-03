05b466b30c72325ca7293c455534a905222e2b75
motinha122 <timota132@gmail.com>
2025-02-02 21:39:04 -0300

    c10 ex00

--- C10/ex00/includes/ft.h
+++ C10/ex00/includes/ft.h
@@ -2,6 +2,17 @@
 #define FT_H
 
 #include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sysexits.h>
 
+#define BUFFER_SIZE 50
+
+#define ZERO_ARGS "File name missing."
+#define MANY_ARGS "Too many arguments."
+#define CANT_READ "Cannot read file."
+
+void print_error(char *str);
 
 #endif
--- C10/ex00/src/main.c
+++ C10/ex00/src/main.c
@@ -1,5 +1,38 @@
 #include "../includes/ft.h"
 
-int main(void){
+int main(int ac, char *argv[])
+{
+  ssize_t read_byte;
+
+  char buffer[BUFFER_SIZE];
+
+  if(ac == 1){
+    print_error(ZERO_ARGS);
+    return EX_NOINPUT;
+  }
+
+  if (ac > 2)
+  {
+    print_error(MANY_ARGS);
+    return EX_USAGE;
+  }
+
+  int fd = open(argv[1], O_RDONLY);
+
+  if(fd < 0){
+    print_error(CANT_READ);
+    return EX_IOERR;
+  }
+
+  while ((read_byte = read(fd, buffer, BUFFER_SIZE)) > 0)
+  {
+    write(1, &buffer, BUFFER_SIZE);
+    bzero(buffer, BUFFER_SIZE);
+  }
+
+  close(fd);
+
+  write(1, "\n", 1);
+
   return 0;
 }
