22a9c33c3ef5aa7c172a0f13c2acb3ea3af02df0
motinha122 <timota132@gmail.com>
2025-02-11 22:35:12 -0300

    tail last 10 lines done

--- C10/ex02/src/print_buffer.c
+++ C10/ex02/src/print_buffer.c
@@ -1,12 +1,14 @@
 #include "../includes/ft.h"
 
 void print_buffer(char *filename){
-
+  printf("== %s ==\n", filename);
   char buffer[BUFFER_SIZE];
 
   int fd = open(filename, O_RDONLY);
   
   ssize_t read_byte = read(fd, buffer, BUFFER_SIZE);
+
+  int char_count = 0;
 
   int lines_count = 1;
 
@@ -14,21 +16,38 @@ void print_buffer(char *filename){
   {
     print_error(CANT_READ);
   }
-
+  
+  // first read (lines and char count)
   while ((read_byte = read(fd, buffer, 1)) > 0)
   {
     if(buffer[0] == '\n'){
       lines_count++;
     }
+    char_count++;
     // write(1, buffer, 1);
     bzero(buffer, BUFFER_SIZE);
   }
+  close(fd);
 
-  if(read_byte != 0){
-    write(1, buffer, BUFFER_SIZE);
+  int line_iterator = lines_count - 9;
+
+  int fd2 = open(filename, O_RDONLY);
+
+  // second read (line iterator)
+  while ((read_byte = read(fd2, buffer, 1)) > 0)
+  {
+    if (buffer[0] == '\n' && line_iterator > 1){
+      line_iterator--;
+    }
+
+    if (line_iterator == 1 || lines_count <= 10)
+    {
+      write(1, buffer, 1);
+      bzero(buffer, 1);
+    }
   }
 
-  printf("lines count: %d \n", lines_count);
+  // printf("char and lines count: %d , %d \n",char_count, lines_count);
   
-  close(fd);
+  close(fd2);
 }
