c2ed9608cb7fc389638ff21a067e4d40f6763dda
motinha122 <timota132@gmail.com>
2025-03-01 07:27:08 -0300

    base erased

--- BASE /ex00/includes/ft.h
+++
@@ -1,8 +0,0 @@
-#ifndef FT_H
-#define FT_H
-
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-
-#endif
--- BASE /ex00/src/main.c
+++
@@ -1,7 +0,0 @@
-#include "../includes/ft.h"
-
-int main(int ac, char *argv[])
-{
- 
-  return 0;
-}
