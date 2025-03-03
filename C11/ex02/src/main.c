c53cd04e9bf345a0bb08078d42c73618bb48e262
motinha122 <timota132@gmail.com>
2025-03-03 18:54:21 -0300

    ft_any added & before function's name

--- C11/ex02/src/main.c
+++ C11/ex02/src/main.c
@@ -3,8 +3,8 @@
 int main(int ac, char *argv[])
 {
   char *tab[] = {"hellO", "999Ab", "XAMPSON", "6575", 0};
-  int res = ft_any(tab, ft_isupper);
-  int res2 = ft_any(tab, ft_islower);
+  int res = ft_any(tab, &ft_isupper);
+  int res2 = ft_any(tab, &ft_islower);
   ft_putnbr(res);
   ft_putnbr(res2);
   return 0;
